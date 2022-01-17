88041f42cd0afd67ccb1c0e22857f1b549d745b0
clintondeacon <31672794+clintondeacon@users.noreply.github.com>
2022-01-17 22:09:14 +0200

    Station resource

--- app/Http/Resources/StationResource.php
+++ app/Http/Resources/StationResource.php
@@ -3,6 +3,8 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Validator;
+use App\Custom\Locations;
 
 class StationResource extends JsonResource
 {
@@ -14,6 +16,7 @@ class StationResource extends JsonResource
      */
     public function toArray($request)
     {
+
         return [
             'id' => $this->id,
             'name' => $this->name,
@@ -23,6 +26,9 @@ class StationResource extends JsonResource
             'address' => $this->address,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
+
         ];
+
     }
+
 }
