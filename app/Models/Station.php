f5cb6f09d804455ca44e3ba9cd96eab7f9c200dc
clintondeacon <31672794+clintondeacon@users.noreply.github.com>
2022-01-17 22:08:17 +0200

    Station model

--- app/Models/Station.php
+++ app/Models/Station.php
@@ -7,4 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Station extends Model
 {
     protected $fillable = ['name','latitude','longitude','company_id','address'];
+
+    public function parent()
+    {
+        return $this->belongsTo('App\Models\Station','id','parent_id')->with('parent');
+    }
+
+    public function children()
+    {
+        return $this->hasMany('App\Models\Station','parent_id','id')->with('children');
+    }
+
 }
