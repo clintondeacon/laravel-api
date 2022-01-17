99081d50a93ea74266cda79c833226d7decd9b6c
clintondeacon <31672794+clintondeacon@users.noreply.github.com>
2022-01-17 22:08:42 +0200

    Company seeder

--- database/seeders/CompanyTableSeeder.php
+++ database/seeders/CompanyTableSeeder.php
@@ -16,9 +16,9 @@ class CompanyTableSeeder extends Seeder
     {
         Company::truncate();
 
-        Company::create(['parent_id' => null, 'name' => "Company A",]);
-        Company::create(['parent_id' => 1, 'name' => "Company B",]);
-        Company::create(['parent_id' => 2, 'name' => "Company C",]);
+        Company::create(['parent_id' => null,"company_ids" => "1,2,3", 'name' => "Company A",]);
+        Company::create(['parent_id' => 1,"company_ids" => "2,3", 'name' => "Company B",]);
+        Company::create(['parent_id' => 2,"company_ids" => "3", 'name' => "Company C",]);
 
 
     }
