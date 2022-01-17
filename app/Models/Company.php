0d53064cd3b2d9ee0fa07e5ab5434ee56364913f
clintondeacon <31672794+clintondeacon@users.noreply.github.com>
2022-01-17 22:08:26 +0200

    Company model

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Company extends Model
 {
     protected $fillable = ['parent_id', 'name'];
+
+    public function parent()
+    {
+        return $this->belongsTo('App\Models\Company','id','parent_id')->with('parent');
+    }
+
+    public function children()
+    {
+        return $this->hasMany('App\Models\Company','parent_id','id')->with('children');
+    }
 }
